5ea56ae1847b167273a79feddf82f44585481f79
Polina Slavyanskaya <polinslvnsk@gmail.com>
2018-06-22 23:11:40 +0300

    add files

---
+++ core/src/ru/geekbrains/stargame/StarGame.java
@@ -0,0 +1,38 @@
+package ru.geekbrains.stargame;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
+public class StarGame extends ApplicationAdapter {
+	SpriteBatch batch;
+	Texture img;
+//	TextureRegion region;
+	
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+		img = new Texture("nebula.jpg");
+//		region = new TextureRegion(img, 10, 10, 100, 100);
+	}
+
+	@Override
+	public void render () {
+		Gdx.gl.glClearColor(0.5f, 0.5f, 0.6f, 0.5f);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		batch.begin();
+		batch.draw(img, 0f, 0f, 700, 500);
+//		batch.draw(region, 50, 50);
+		batch.end();
+	}
+	
+	@Override
+	public void dispose () {
+		batch.dispose();
+		img.dispose();
+
+	}
+}
